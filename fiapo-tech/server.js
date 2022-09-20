4987854e359dbe7a8df23ab48212d050b6d1e858
Junior Prado <juniorcvnnn@gmail.com>
2022-09-20 11:15:52 -0300

    Configurações feitas para backend

--- fiapo-tech/server.js
+++ fiapo-tech/server.js
@@ -1,96 +1,98 @@
-const path=require('path');
-const express=require('express');
+//aplicação em back
 
-const app=express();
+// const path=require('path');
+// const express=require('express');
 
-app.use(require('cors')())
+// const app=express();
 
-app.use(express.json())
+// app.use(require('cors')())
 
-const upload=require('multer')();
+// app.use(express.json())
+
+// const upload=require('multer')();
  
 
-app.use(express.static(path.join(__dirname,'build')))
+// app.use(express.static(path.join(__dirname,'build')))
 
-const nodemailer=require('nodemailer');
-const { resolve } = require('path');
-const { rejects } = require('assert');
+// const nodemailer=require('nodemailer');
+// const { resolve } = require('path');
+// const { rejects } = require('assert');
 
-app.post('/send',upload.single('file'),(req,res)=>{
-    const name=req.body.name;
-    const email=req.body.email;
-    const tel=req.body.tel;
-    const message=req.body.message;
-    const file=req.file; 
-    // console.log("arquivo",file);
-    const dataPedido=new Date();
+// app.post('/send',upload.single('file'),(req,res)=>{
+//     const name=req.body.name;
+//     const email=req.body.email;
+//     const tel=req.body.tel;
+//     const message=req.body.message;
+//     const file=req.file; 
+//     // console.log("arquivo",file);
+//     const dataPedido=new Date();
 
-    let smtp=nodemailer.createTransport({
-        host:'smtp.office365.com',
-        port:587,
-        secure:false,
-        auth:{
-            user:"fiapo.assistente@hotmail.com",
-            pass:"Fia123321."
-        }
-    })
-    
-    const mail={
-        from:"Assistente <fiapo.assistente@hotmail.com>",
-        to:'fiapotech@gmail.com',
-        subject:`Novo pedido! (${dataPedido.getDate()})`,
-        text:`
-        Cliente: ${name}
-        Email: ${email}
-        Telefone: ${tel}
-        _________________________________________
-        
-        Decrição do pedido: ${message}
-        _________________________________________
-        `,
-    }
-    if(file){
-        mail.attachments= [];
-        mail.attachments.push(
-            {
-                filename: file.originalname,
-                content:file.buffer
-            }
-            )
-    }
-    return new Promise((resolve,reject)=>{
-        resolve(
-            smtp.sendMail(mail,(err,result)=>{
-                if(err){
-                    // console.log(err);
-                    smtp.close()
-                    return(err)
-                }else{
-                    
-                    // alert("a")
-                    smtp.close()
-                    console.log(result);
-                    return(result)
-                }
-            })
-        )
-    }).then((result)=>{
-        return(result);
-    }
-    )
-    // return new Promise((resolve,reject)=>{
-
-//     require('./src/service/serviceEmail')(name,email,tel,message,file)
-//     .then(response=>{
-//         console.log("rep: ",response);
-//         res.json(response);
+//     let smtp=nodemailer.createTransport({
+//         host:'smtp.office365.com',
+//         port:587,
+//         secure:false,
+//         auth:{
+//             user:"fiapo.assistente@hotmail.com",
+//             pass:"Fia123321."
+//         }
 //     })
-//     .catch(err=>{
-//         console.log("erro: ",err);
-//         res.status(500).json(err)});
-// })
-})
+    
+//     const mail={
+//         from:"Assistente <fiapo.assistente@hotmail.com>",
+//         to:'fiapotech@gmail.com',
+//         subject:`Novo pedido! (${dataPedido.getDate()})`,
+//         text:`
+//         Cliente: ${name}
+//         Email: ${email}
+//         Telefone: ${tel}
+//         _________________________________________
+        
+//         Decrição do pedido: ${message}
+//         _________________________________________
+//         `,
+//     }
+//     if(file){
+//         mail.attachments= [];
+//         mail.attachments.push(
+//             {
+//                 filename: file.originalname,
+//                 content:file.buffer
+//             }
+//             )
+//     }
+//     return new Promise((resolve,reject)=>{
+//         resolve(
+//             smtp.sendMail(mail,(err,result)=>{
+//                 if(err){
+//                     // console.log(err);
+//                     smtp.close()
+//                     return(err)
+//                 }else{
+                    
+//                     // alert("a")
+//                     smtp.close()
+//                     console.log(result);
+//                     return(result)
+//                 }
+//             })
+//         )
+//     }).then((result)=>{
+//         return(result);
+//     }
+//     )
+//     // return new Promise((resolve,reject)=>{
 
-app.listen(3000,()=>{
-    console.log('server start');
-})
+// //     require('./src/service/serviceEmail')(name,email,tel,message,file)
+// //     .then(response=>{
+// //         console.log("rep: ",response);
+// //         res.json(response);
+// //     })
+// //     .catch(err=>{
+// //         console.log("erro: ",err);
+// //         res.status(500).json(err)});
+// // })
+// })
+
+// app.listen(3000,()=>{
+//     console.log('server start');
+// })
